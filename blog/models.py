b1b23dbe47135de62d076d7fceab42482507f6f7
Kristine Kamola <kristine.kamola@gmail.com>
2025-01-16 14:47:12 +0000

    fixing errors

--- blog/models.py
+++ blog/models.py
@@ -5,11 +5,13 @@ from cloudinary.models import CloudinaryField
 STATUS = ((0, "Draft"), (1, "Published"))
 
 # Create your models here.
+
+
 class Post(models.Model):
     title = models.CharField(max_length=200, unique=True)
     slug = models.SlugField(max_length=200, unique=True)
     author = models.ForeignKey(
-    User, on_delete=models.CASCADE, related_name="blog_posts"
+        User, on_delete=models.CASCADE, related_name="blog_posts"
     )
     featured_image = CloudinaryField('image', default='placeholder')
     content = models.TextField()
@@ -22,22 +24,20 @@ class Post(models.Model):
         ordering = ["-created_on"]
 
     def __str__(self):
-        return f"The title of this post is {self.title} | written by {self.author}"
+        return f"{self.title} | written by {self.author}"
 
 
 class Comment(models.Model):
-    post = models.ForeignKey(
-        Post, on_delete=models.CASCADE, related_name="comments")
+    post = models.ForeignKey(Post, on_delete=models.CASCADE,
+                             related_name="comments")
     author = models.ForeignKey(
-        User, on_delete=models.CASCADE, related_name="commenter"
-    )
-    body = models.TextField(max_length=200)
-    approved = models.BooleanField(default=False)
+        User, on_delete=models.CASCADE, related_name="commenter")
+    body = models.TextField()
     created_on = models.DateTimeField(auto_now_add=True)
-    
+    approved = models.BooleanField(default=False)
+
     class Meta:
         ordering = ["created_on"]
 
     def __str__(self):
         return f"Comment {self.body} by {self.author}"
-    
